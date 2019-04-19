46a0346bbe7b49db81d5577f17df7fe6b00a3929
huiqing <huiqingw2017@gmail.com>
2019-04-18 19:15:10 -0500

    modify gui

--- autokeras/gui/resnet_gui.py
+++ autokeras/gui/resnet_gui.py
@@ -5,67 +5,66 @@ class resnet_gui:
     def __init__(self):
         root = Tk()
         theGui = gui(root)
-        root.geometry("500x500")  # You want the size of the app to be 500x500
+        root.geometry("600x400")  # You want the size of the app to be 500x500
         root.resizable(0, 0)  # Don't allow resizing in the x or y direction
         root.title('Gui for resnet')
         root.mainloop()
-        self.var = theGui.var
-        print(self.var)
 
 
 class gui:
     def __init__(self, master):
         self.master = master
+
         self.var = []
-        Label(self.master, text='Set your parameters of resnet').pack()
+
+        label = Label(self.master, text='Set your parameters of resnet', font=("Arial", 18))
+        # label.config(font=("Arial", 20))
+        label.pack()
         frame1 = Frame(self.master)
         frame1.pack()
+        self.btn2 = IntVar()
 
-        self.btn1 = StringVar()
-        self.btn2 = StringVar()
-
-        Label(frame1, text='Number of Layers').grid(row=1, column=0)
-        subframe1 = Frame(frame1)
-        subframe1.grid(row=1, column=1)
-        radio1_1 = Radiobutton(subframe1, text='18', value=18, variable=self.btn1).pack(side=LEFT)
-        radio1_2 = Radiobutton(subframe1, text='34', value=34, variable=self.btn1).pack(side=LEFT)
-        radio1_3 = Radiobutton(subframe1, text='50', value=50, variable=self.btn1).pack(side=LEFT)
-        radio1_4 = Radiobutton(subframe1, text='101', value=101, variable=self.btn1).pack(side=LEFT)
-        radio1_5 = Radiobutton(subframe1, text='152', value=152, variable=self.btn1).pack(side=LEFT)
-
-        Label(frame1, text='Size of filters').grid(row=2, column=0)
+        Label(frame1, text='Block Design', font=("", 12)).grid(row=2, column=0)
         subframe2 = Frame(frame1)
         subframe2.grid(row=2, column=1)
-        radio2_1 = Radiobutton(subframe2, text='2*2', value=2, variable=self.btn2).pack(side=LEFT)
-        radio2_2 = Radiobutton(subframe2, text='3*3', value=3, variable=self.btn2).pack(side=LEFT)
-        radio2_3 = Radiobutton(subframe2, text='4*4', value=4, variable=self.btn2).pack(side=LEFT)
+        radio2_1 = Radiobutton(subframe2, text='Basic', value=0, variable=self.btn2, font=("", 12)).pack(side=LEFT)
+        radio2_2 = Radiobutton(subframe2, text='Bottleneck', value=1, variable=self.btn2, font=("", 12)).pack(side=LEFT)
 
-        Label(frame1, text='Learning rate').grid(row=3, column=0)
-        self.rateEntry = Entry(frame1)
-        self.rateEntry.grid(row=3, column=1)
+        frame2 = Frame(self.master)
+        frame2.pack()
+        Label(frame1, text='Repetitions of Conv Block', font=("", 12)).grid(row=3, column=0, rowspan=50)
+        subframe1 = Frame(frame2)
 
+        default_r = StringVar()
+        default_r.set('2')
+        self.conv2_x = Entry(subframe1, textvariable=default_r)
+        self.conv2_x.grid(row=3, column=1, columnspan=20)
+        self.conv3_x = Entry(subframe1, textvariable=default_r)
+        self.conv3_x.grid(row=4, column=1, columnspan=20)
+
+        self.conv4_x = Entry(subframe1, textvariable=default_r)
+        self.conv4_x.grid(row=5, column=1, columnspan=20)
+
+        self.conv5_x = Entry(subframe1, textvariable=default_r)
+        self.conv5_x.grid(row=6, column=1, columnspan=20)
+
+        subframe1.grid(row=3, column=0)
         frame3 = Frame(self.master)
         frame3.pack()
-        button = Button(frame3, text='Run network', width=10)
+        button = Button(frame3, text='Run network', width=10, font=("", 12))
         button.bind('<Button-1>', self.get_variable)
         button.pack()
 
-        frame4 = Frame(self.master)
-        frame4.pack()
-        Label(frame4, text='show the network architecture').pack(side=LEFT)
-
-        self.quitBtn = Button(self.master, text="Quit", command=self.master.quit).pack()
+        self.quitBtn = Button(frame3, text="Quit", command=self.master.quit, font=("", 12)).pack()
         # get parameters of resnet
 
     def get_variable(self, event):
-
-        self.var.append(int(self.btn1.get()))
+        self.var.append(int(self.conv2_x.get()))
+        self.var.append(int(self.conv3_x.get()))
+        self.var.append(int(self.conv4_x.get()))
+        self.var.append(int(self.conv5_x.get()))
         self.var.append(int(self.btn2.get()))
-        self.var.append(float(self.rateEntry.get()))
-        # res_range = int(rangeEntry.get())
 
-        print("Reading inputs from gui: ...")
-
-        # print(var)
+        print(self.var)
 
 
